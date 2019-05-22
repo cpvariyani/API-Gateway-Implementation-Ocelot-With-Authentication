6dce6c4b097e90dd33a2cfa81ccefc36786da1f5
cpvariyani <cpvariyani90@gmail.com>
2019-05-22 10:47:07 +0530

    files which has the authentication
    ocelot.json files and authentication files.

---
+++ ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace product_ms.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("getProduct")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Product product = GetDummyData();
+            return Ok(product);
+        }
+
+
+        private Product GetDummyData()
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Alexa",
+                Price = "$98"
+            };
+            return product;
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Price { get; set; }
+        }
+
+    }
+}
